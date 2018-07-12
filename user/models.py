a83155624a709300a90019dd8e73398fd44f147a
김동욱 <gimdong-ug@gimdong-ug-ui-MacBook-Air.local>
2018-07-12 14:08:43 +0900

    user model_3

--- user/models.py
+++ user/models.py
@@ -7,9 +7,7 @@ class User(AbstractUser):
   created = models.DateTimeField(auto_now_add=True)
   updated = models.DateTimeField(auto_now=True)
   address = models.CharField(max_length=100, blank=True, null=True)
-  contact = models.CharField(max_length=11, blank=True, null=True)
+  contact = models.CharField(max_length=20, blank=True, null=True)
   confirmationToken = models.CharField(max_length=100, default='')
-
-
   class Meta:
     ordering = ('id',)
