affd7f8a784b77eff15f01193d2aaf66f32edaca
김동욱 <gimdong-ug@gimdong-ug-ui-MacBook-Air.local>
2018-07-17 14:42:36 +0900

    d

--- item/urls.py
+++ item/urls.py
@@ -3,11 +3,11 @@ from django.conf.urls import url
 from item import views
 
 urlpatterns = [
-  url(r'^losts$', views.lostlist),
+  url(r'^losts$', views.lostList),
   url(r'^lost/(?P<pk>[0-9]+)$', views.lostDetail),
   url(r'^lost/(?P<pk>[0-9]+)/complete$', views.complete_lost),
 
-  url(r'^founds$', views.foundlist),
+  url(r'^founds$', views.foundList),
   url(r'^found/(?P<pk>[0-9]+)$', views.foundDetail),
   url(r'^found/(?P<pk>[0-9]+)/complete$', views.complete_found),
 ]
--- item/views.py
+++ item/views.py
@@ -14,7 +14,7 @@ from django.http import QueryDict
 
 @api_view(['POST','GET'])
 @permission_classes((IsAuthenticated,))
-def lostlist(request):
+def lostList(request):
   if request.method == 'POST':
     dictData = request.data.dict()
     dictData['user']=request.user.id
@@ -29,10 +29,10 @@ def lostlist(request):
       )
     print(serializer.errors)
     return Response(
-      data={'message':'오류'},
+      data = {'message':'오류'},
       status = status.HTTP_403_FORBIDDEN,
     )
-  elif request.method =='GET':
+  elif request.method == 'GET':
     queryset = Lost.objects.all()
     serializers = LostSerializer(queryset, many=True)
     print(serializers.data)
@@ -47,7 +47,7 @@ def lostDetail(request,pk):
     return Response(
     data={'message':'권한이 없습니다'}
   )
-  if request.method =='GET':
+  if request.method == 'GET':
     lost_serializer = LostSerializer(lost)
     return Response(lost_serializer.data)
   elif request.method == 'DELETE':
@@ -88,25 +88,25 @@ def complete_lost(request,pk):
 
 @api_view(['POST','GET'])
 @permission_classes((IsAuthenticated,))
-def foundlist(request):
+def foundList(request):
   if request.method == 'POST':
     dictData = request.data.dict()
-    dictData['user']=request.user.id
+    dictData['user'] = request.user.id
     modifiedQueryDict = QueryDict('',mutable =True)
     modifiedQueryDict.update(dictData)
     serializer = FoundSerializer(data=modifiedQueryDict)
     if serializer.is_valid():
       serializer.save()
       return Response(
-        data={'message': 'Found list에 추가되었습니다'},
+        data = {'message': 'Found list에 추가되었습니다'},
         status = status.HTTP_200_OK,
       )
     print(serializer.errors)
     return Response(
-      data={'message':'오류'},
+      data = {'message':'오류'},
       status = status.HTTP_403_FORBIDDEN,
     )
-  elif request.method =='GET':
+  elif request.method == 'GET':
     queryset = Found.objects.all()
     serializers = FoundSerializer(queryset, many=True)
     print(serializers.data)
@@ -119,9 +119,9 @@ def foundDetail(request,pk):
   found = Found.objects.get(pk=pk)
   if user != found.user:
     return Response(
-    data={'message':'권한이 없습니다'}
+    data = {'message':'권한이 없습니다'}
   )
-  if request.method =='GET':
+  if request.method == 'GET':
     found_serializer = FoundSerializer(found)
     return Response(found_serializer.data)
   elif request.method == 'DELETE':
@@ -131,7 +131,7 @@ def foundDetail(request,pk):
     )
   elif request.method == 'PUT':
     dictData = request.data.dict()
-    dictData['user']=request.user.id
+    dictData['user'] = request.user.id
     modifiedQueryDict = QueryDict('',mutable =True)
     modifiedQueryDict.update(dictData)
     serializer = FoundSerializer(found, data=modifiedQueryDict)
